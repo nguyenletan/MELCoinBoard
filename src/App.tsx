22389275ccd02771fad8558244047712c08bbf34
Tan Nguyen Le <nguyenletan@outlook.com>
2022-02-12 18:01:14 +1100

    fixed cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Coins from './pages/Coins';
@@ -9,27 +9,28 @@ import News from './pages/News';
 import CoinDetail from './pages/CoinDetail';
 
 
-
 function App() {
-  useEffect(() => {
-    fetch('https://db.pokemongohub.net/api/pokemon/with-type/ghost?locale=en-US').then(x => x.json()).then(x => {
-      console.log(x)
-      return x;
-    })
-  }, []);
+    // useEffect(() => {
+    //     fetch('https://mel-cors-api.herokuapp.com/https://db.pokemongohub.net/api/pokemon/with-type/ghost?locale=en-US')
+    //         .then(x => x.json())
+    //         .then(x => {
+    //             console.log(x)
+    //             return x;
+    //         })
+    // }, []);
 
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="coins" element={<Coins />} />
-        <Route path="coins/:id" element={<CoinDetail />} />
-        <Route path="exchanges" element={<Exchanges />} />
-        <Route path="news" element={<News />} />
-        <Route path="*" element={<div>Not Found</div>} />
-      </Routes>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="coins" element={<Coins/>}/>
+                <Route path="coins/:id" element={<CoinDetail/>}/>
+                <Route path="exchanges" element={<Exchanges/>}/>
+                <Route path="news" element={<News/>}/>
+                <Route path="*" element={<div>Not Found</div>}/>
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
